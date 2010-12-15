c7fc30c514f70488b67b35acd18c3423651be06a
Taylor Jones <sqlteam2010@gmail.com>
2010-12-15 16:49:13 -0700

    update dbconnect.php for local server

--- admin/dbconnect.php
+++ admin/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 ini_set("display_errors","1"); // sql112.0fees.net:3306 fees0_6433866 sqlteam
-$connection = mysql_connect('sql112.0fees.net:3306','fees0_6433866','sqlteam');
+$connection = mysql_connect('localhost','root','sqlteam');
 if (!$connection)
   {
   die('Sorry Could Not Connect To Database Please Contact The Administrator.' . mysql_error());
